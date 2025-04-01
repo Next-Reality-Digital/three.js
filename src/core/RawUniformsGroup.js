b4b522a0ab4354b42374e0c4aa7cdb619d4138a8
Angel Cabrera <acabrera@nextreality.digital>
2025-04-01 17:43:47 +0000

    feat: convert raw uniforms group to es6 classes

--- src/core/RawUniformsGroup.js
+++ src/core/RawUniformsGroup.js
@@ -1,53 +1,58 @@
-import { UniformsGroup } from './UniformsGroup.js';
+import { UniformsGroup } from "./UniformsGroup.js"; // Assuming this path is correct relative to your file
 
 /**
  * @author robertlong / https://github.com/robertlong
+ * @author WestLangley / https://github.com/WestLangley (Updated to ES6 class)
  */
 
-function RawUniformsGroup( data ) {
+class RawUniformsGroup extends UniformsGroup {
+	constructor(data) {
+		// Call the parent class constructor (UniformsGroup)
+		super();
 
-	UniformsGroup.call( this );
+		// Set the flag to identify this type
+		this.isRawUniformsGroup = true;
 
-	this.data = data;
-	this.autoUpdate = true;
-	this.needsUpdate = false;
-
-}
-
-RawUniformsGroup.prototype = Object.assign( Object.create( UniformsGroup.prototype ), {
-
-	constructor: RawUniformsGroup,
-
-	isRawUniformsGroup: true,
-
-	add: function ( _uniform ) {
-
-		console.warn( 'THREE.RawUniformsGroup: .add() is unimplemented. Modify .data manually instead.' );
-
-		return this;
-
-	},
-
-	remove: function ( _uniform ) {
-
-		console.warn( 'THREE.RawUniformsGroup: .add() is unimplemented. Modify .data manually instead.' );
-
-		return this;
-
-	},
-
-	copy: function ( source ) {
-
-		UniformsGroup.prototype.copy.call( this );
-
-		this.autoUpdate = source.autoUpdate;
-		this.data = source.data.slice( 0 );
-
-		return this;
-
+		// Initialize RawUniformsGroup specific properties
+		this.data = data;
+		this.autoUpdate = true;
+		this.needsUpdate = false; // This seems redundant if autoUpdate is true, but kept for compatibility
 	}
 
-} );
+	// Override add method - Note: It was unimplemented before
+	add(_uniform) {
+		console.warn(
+			"THREE.RawUniformsGroup: .add() is unimplemented. Modify .data manually instead."
+		);
+		return this;
+	}
 
+	// Override remove method - Note: It was unimplemented before
+	remove(_uniform) {
+		// Original had a typo in the warning message, referencing .add()
+		console.warn(
+			"THREE.RawUniformsGroup: .remove() is unimplemented. Modify .data manually instead."
+		);
+		return this;
+	}
+
+	// Override copy method
+	copy(source) {
+		// Call the parent class's copy method first
+		super.copy(source);
+
+		// Copy RawUniformsGroup specific properties
+		this.autoUpdate = source.autoUpdate;
+
+		// Make sure source.data exists and is sliceable before copying
+		// Use slice(0) for a shallow copy of the array/typed array if applicable
+		this.data =
+			source.data && typeof source.data.slice === "function"
+				? source.data.slice(0)
+				: source.data;
+
+		return this;
+	}
+}
 
 export { RawUniformsGroup };
